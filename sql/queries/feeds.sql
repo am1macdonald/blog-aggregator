70f16b579d3c397fe30d525571c91f2b3096df14
Adam MacDonald <am1macdonald@gmail.com>
2024-04-18 21:01:10 -0400

    Add feed operations

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -5,3 +5,22 @@ RETURNING *;
 
 -- name: GetAllFeeds :many
 SELECT * FROM feeds;
+
+-- name: GetNextFeedsToFetch :many
+SELECT *
+FROM (SELECT *
+      FROM feeds
+      WHERE last_fetched_at IS NULL
+      UNION
+      SELECT *
+      FROM feeds
+      WHERE last_fetched_at IS NOT NULL
+      ORDER BY last_fetched_at DESC) AS T1
+LIMIT $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds SET
+updated_at = NOW(),
+last_fetched_at = NOW()
+WHERE id = $1;
+
